acdc7fec3edb46275e396067ad0cf0b1191c9767
horatiubran <bran.horatiu@gmail.com>
2022-09-05 12:37:52 +0200

    exercising commits

--- src/components/pricing/index.js
+++ src/components/pricing/index.js
@@ -2,5 +2,6 @@ export const Pricing = () => {
     return <div>
         <p>Prices are the best in town.</p>
         <p>We have an commercial excess of 0-500%</p>
+        <p>Delivery Fee is 20$</p>
     </div>
 }
